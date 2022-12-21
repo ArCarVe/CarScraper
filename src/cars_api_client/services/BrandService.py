aeafad9b043e548b2ad0752e68108fafc831550d
gabrielmvas <gabrielmvas@gmail.com>
2022-12-21 16:57:53 -0300

    [refactor] create single method responsible for making requests

--- src/cars_api_client/services/BrandService.py
+++ src/cars_api_client/services/BrandService.py
@@ -1,14 +1,7 @@
-import requests
 from cars_api_client.services.Service import Service
-from json import loads
 
 
 class BrandService(Service):
 
     def post(self, brand, uri="/brands"):
-        response = requests.post(
-            url=f"{self.url}{uri}",
-            json=brand
-        )
-
-        return response.json()
+        return self.post_request(uri, brand)
